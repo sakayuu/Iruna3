fa4075035868ae03c3e4def384b6b841fa3dd6ab
OverdrIV_1203 <sakai1203yuu@gmail.com>
2021-02-16 07:30:06 +0900

    add.とりあえず今までの作業をup

---
+++ Iruna3/Assets/Scripts/Character/Character.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Data;
+
+public class Character
+{
+    Status status;
+
+    string name;
+
+}
---
+++ Iruna3/Assets/Scripts/GamePlayManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayManager : MonoBehaviour
+{
+    [SerializeField, Header("プレイヤー")]
+    public GameObject player;
+    [SerializeField, Header("UI")]
+    public GameObject UI;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
