e2ba24adf04180e8ef9e3fa0dc6bb258dc9e3548
OverdrIV_1203 <sakai1203yuu@gmail.com>
2021-02-20 23:51:22 +0900

    add.シーン移行の基礎を追加

---
+++ Iruna3/Assets/Scripts/TitleManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    [SerializeField, Header("スタート")]
+    GameObject start;
+    [SerializeField, Header("クレジット")]
+    GameObject credit;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ButtonCheck();
+    }
+
+    void ButtonCheck()
+    {
+        if (start.GetComponent<button>().ClickFlag)
+            SceneManager.LoadScene("CharaChoiceScene");
+    }
+}
